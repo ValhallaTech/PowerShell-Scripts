b88f89b296b784fe21e7847f6adbd725db4e604e
Fred <valhallatechnc@gmail.com>
2023-02-10 14:12:11 -0500

    Create Edit-BIOSConfig.ps1

---
+++ Edit-BIOSConfig.ps1
@@ -0,0 +1,37 @@
+# General variables
+$systemDrive = $env:SystemDrive
+$pathLaptopBIOSExe = "$systemDrive\Tanium_Software\BIOSConf-Laptop_x64"
+$pathDesktopBIOSExe = "$systemDrive\Tanium_Software\BIOSConf-Desktop_x64"
+$rkSecureBoot = "HKLM:\SYSTEM\CurrentControlSet\Control\SecureBoot\State"
+$valSecureBoot = Get-ItemPropertyValue -Path $rkSecureBoot -Name UEFISecureBootEnabled
+
+# Conditional variables that will set the parameters that will be checked later
+$isLaptop = [bool](Get-WmiObject -Class Win32_SystemEnclosure |
+    Where-Object ChassisTypes -in '{9}', '{10}', '{14}')
+$chassisType = if ($isLaptop -eq $true){
+    "Laptop"}
+else {"Desktop"
+}
+$statusSecureBoot = if ( $valSecureBoot -eq 1) {
+    $true
+}
+else {
+    $false
+}
+
+# Output parameters
+Write-Host "***" "" "This machine is a $chassisType" "" "***"
+if ($statusSecureBoot -eq $true) {
+    Write-Host "***" "" "BIOS Configuration: Secure boot is on" "" "***"
+}
+else {
+    Write-Host "***" "" "BIOS Configuration: Secure boot is off" "" "***"
+}
+
+# Check parameters to determine if the .exe needs to run to re-configure the BIOS
+if ($chassisType -eq "Laptop" -AND $statusSecureBoot -eq $false ) {
+    $pathLaptopBIOSExe
+}
+elseif ($chassisType -eq "Desktop" -AND $statusSecureBoot -eq $false) {
+    $pathDesktopBIOSExe
+}
