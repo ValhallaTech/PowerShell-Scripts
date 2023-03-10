08c8442385c0d25c6a288f65cd4a6e42ea64bb1d
Fred <valhallatechnc@gmail.com>
2023-03-10 13:21:42 -0500

    Update Map-GroupDrive.ps1
    Updates variables into a splat array

--- Map-GroupDrive.ps1
+++ Map-GroupDrive.ps1
@@ -1,6 +1,11 @@
-# General Variables
-$cred = Get-Credential
-$pathGroupDrive = "\\wssu.edu\ramfile\groups"
+# Map group drive using these parameters
+$paramMapping = @{
+  Name = "G"
+  PSProvider = "FileSystem"
+  Root = "\wssu.edu\ramfile\groups"
+  Persist = $true
+  Credential = (Get-Credential)
+}
 
 # Map group drive using the submitted credentials
-New-PSDrive -Name "G" -PSProvider "FileSystem" -Root $pathGroupDrive -Persist -Credential $cred
+New-PSDrive @paramMapping
