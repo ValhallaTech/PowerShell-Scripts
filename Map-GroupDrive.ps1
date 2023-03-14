341eeb5c3035acbb6569adc40eaa8ca1f842f8b8
Fred <valhallatechnc@gmail.com>
2023-03-14 13:25:37 -0400

    Update Map-GroupDrive
    Add logging functionality with PoShLog
    Update the path to the group share
    Update comment for the New-PSDrive process

--- Map-GroupDrive.ps1
+++ Map-GroupDrive.ps1
@@ -1,11 +1,25 @@
+# Grouper Mapper 2.0.0
+
+# Check if PoShLog module is installed and if not install the current version
+if (-not $chkPoShLog) {
+    Install-Module -Name $modPoShLog
+}
+    Write-Log -Logger $logger -Message "PoShLog module installed"
+else {
+    Write-Log -Logger $logger -Message "PoShLog module already installed"
+}
+    # Import PoShLog module
+Import-Module -Name $modPoShLog
+    Write-Log -Logger $logger -Message "PoShLog module imported"
+
 # Map group drive using these parameters
 $paramMapping = @{
   Name = "G"
   PSProvider = "FileSystem"
-  Root = "\wssu.edu\ramfile\groups"
+  Root = "\\wssufs2\ramfile\groups"
   Persist = $true
   Credential = (Get-Credential)
 }
 
-# Map group drive using the submitted credentials
+# Map group drive using the specified parameters
 New-PSDrive @paramMapping
