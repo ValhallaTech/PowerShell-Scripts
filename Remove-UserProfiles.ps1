9d540d97a43d9af4493d066d811112c833cd077e
Fred <valhallatechnc@gmail.com>
2023-03-14 13:26:11 -0400

    Update Remove-UserProfiles.ps1
    Update comments to be more accurate in their descriptions

--- Remove-UserProfiles.ps1
+++ Remove-UserProfiles.ps1
@@ -1,6 +1,6 @@
-# Variable that creates an array of the accounts to be excluded from removal process
+# Variables that creates an array of the accounts to be excluded from removal process
 $acctsExcluded = "ramadmin"
 $localHost = HOSTNAME.EXE
 
-# Method that will delete all of the user profiles on a computer, except the accounts listed in the above array
+# Method that will delete all of the user profiles on a computer, except the accounts listed in the $acctsExcluded variable
 Get-CimInstance -ComputerName $localHost -Class Win32_UserProfile | Where-Object { $_.LocalPath.split('\')[-1] -notin $acctsExcluded } | Remove-CimInstance
