735eb98e4e08397f710ce4dffa7d097d99f61cc8
Fred <valhallatechnc@gmail.com>
2023-04-10 10:02:43 -0400

    Update Remove-UserProfiles.ps1
    Update account exclusions to include built-in accounts
    Add method to handle null profile paths

--- Remove-UserProfiles.ps1
+++ Remove-UserProfiles.ps1
@@ -1,6 +1,5 @@
-# Variables that creates an array of the accounts to be excluded from removal process
-$acctsExcluded = "ramadmin"
-$localHost = HOSTNAME.EXE
+# Variables that create an array of the accounts to be excluded from removal process
+$acctsExcluded = @("ramadmin", "S-1-5-18", "S-1-5-19", "S-1-5-20")
 
 # Method that will delete all of the user profiles on a computer, except the accounts listed in the $acctsExcluded variable
-Get-CimInstance -ComputerName $localHost -Class Win32_UserProfile | Where-Object { $_.LocalPath.split('\')[-1] -notin $acctsExcluded } | Remove-CimInstance
+Get-CimInstance -Class Win32_UserProfile | Where-Object { ($_.LocalPath -ne $null) -and ($_.LocalPath.split('\')[-1] -notin $acctsExcluded) -and ($_.SID -notin $acctsExcluded) } | Remove-CimInstance
