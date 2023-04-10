8c4e04a427389124aa7eca6b1c63d2b8c591823c
Fred <valhallatechnc@gmail.com>
2023-04-10 10:21:03 -0400

    Update Remove-UserProfiles.ps1
    Add a function to check if the service WinRM is running. If it isn't then it will run a command to configure and start it

--- Remove-UserProfiles.ps1
+++ Remove-UserProfiles.ps1
@@ -1,3 +1,22 @@
+# User Profile Sweeper 1.0.2
+
+# Function to check if WinRM service is running and configured
+function Invoke-WinRM {
+    $winrmService = Get-Service -Name WinRM
+    $winrmCommand = cmd.exe /c "$WinRM quickconfig -quiet"
+    if ($winrmService.Status -ne "Running") {
+        Write-Host "WinRM service is not running. Configuring..."
+        try {
+            $winrmCommand
+            Write-Host "WinRM has been configured successfully."
+        } catch {
+            Write-Error "An error occurred while configuring WinRM: $_"
+        }
+    } else {
+        Write-Host "WinRM service is running."
+    }
+}
+
 # Variables that create an array of the accounts to be excluded from removal process
 $acctsExcluded = @("ramadmin", "S-1-5-18", "S-1-5-19", "S-1-5-20")
 
