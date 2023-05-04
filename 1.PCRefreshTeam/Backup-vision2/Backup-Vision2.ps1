5e6f43d96da6789aaf8c5c6f7930dfa7503a7b2b
Fred <valhallatechnc@gmail.com>
2023-05-04 15:30:39 -0400

    Update Backup-Vision2.ps1
    Fix the configuration for PoShLog
    Change the name of the log file that is saved
    Change the Write-Log instances to Write-InfoLog
    Change the error message to use Write-ErrorLog

--- 1.PCRefreshTeam/Backup-vision2/Backup-Vision2.ps1
+++ 1.PCRefreshTeam/Backup-vision2/Backup-Vision2.ps1
@@ -10,35 +10,41 @@ $chkRobocopy = Get-Module -Name $modRobocopy
 $modPoShLog = "PoShLog"
 $chkPoShLog = Get-Module -Name $modPoShLog
 
+# Check if PoShLog module is installed and if not install the current version
+if (-not $chkPoShLog) {
+    Install-Module -Name $modPoShLog
+}
 
-# Configure PoShLog
-$LogFilePath = "C:\Logs\Robocopy.log"
-$Log = New-Log -Name Robocopy -FilePath $LogFilePath
+# Import PoShLog module
+Import-Module -Name $modPoShLog
 
-try {
-        # Check if PoShLog module is installed and if not install the current version
-    if (-not $chkPoShLog) {
-        Install-Module -Name $modPoShLog
-    }
-        Write-Log -Logger $logger -Message "PoShLog module installed"
-    else {
-        Write-Log -Logger $logger -Message "PoShLog module already installed"
-    }
-        # Import PoShLog module
-    Import-Module -Name $modPoShLog
-        Write-Log -Logger $logger -Message "PoShLog module imported"
+# Configure variables for logging
+$logFileName = "Backup-Vision2.log"
+$logFilePath = "$systemDrive\Logs\$logFileName"
+
+# Create logger instance
+$logger = New-Logger
+
+# Configure logger instance
+$logger |
+    Set-MinimumLevel -Value Information |
+    Add-SinkFile -Path $logFilePath |
+    Add-SinkConsole |
+    Start-Logger
+
+Write-InfoLog "PoShLog module imported and logger configured"
 
     # Check if RobocopyPS module is installed and if not install the current version
     if (-not $chkRobocopy) {
         Install-Module -Name $modRobocopy
     }
-        Write-Log -Logger $logger -Message "RobocopyPS module installed"
+        Write-InfoLog -Logger $logger -Message "RobocopyPS module installed"
     else {
-        Write-Log -Logger $logger -Message "RobocopyPS module already installed"
+        Write-InfoLog -Logger $logger -Message "RobocopyPS module already installed"
     }
         # Import RobocopyPS module
     Import-Module -Name $modRobocopy
-        Write-Log -Logger $logger -Message "RobocopyPS module imported"
+        Write-InfoLog -Logger $logger -Message "RobocopyPS module imported"
 
     # Create parameters for Robocopy
     $paramRobo = @{
@@ -51,12 +57,13 @@ try {
         Verbose = $true
     }
 
+try {
     # Execute Robocopy command
-    Write-Log -Logger $Log -Message "Executing Robocopy command"
+    Write-InfoLog -Logger $Log -Message "Executing Robocopy command"
     Invoke-RoboCopy @paramRobo -DirectoryCopyFlags D,A,T -ExcludeFileName *.DAT*,*.dll* -ExcludeDirectory *OneDrive*,*AppData*,*Application*
-    Write-Log -Logger $Log -Message "Robocopy command completed successfully"
+    Write-InfoLog -Logger $Log -Message "Robocopy command completed successfully"
 
 } catch {
     # Log any errors that occur
-    Write-Log -Logger $Log -Level Error -Message $_.Exception.Message
+    Write-ErrorLog -Logger $Log -Level Error -Message $_.Exception.Message
 }
