2bcee0dcb026eefb534666eaba6a526f65c52a5f
Fred <valhallatechnc@gmail.com>
2023-06-19 09:51:29 -0400

    Update Map-GroupDrive.ps1
    Update the logging function to use the PoShLog module

--- Map-GroupDrive.ps1
+++ Map-GroupDrive.ps1
@@ -1,5 +1,37 @@
 $systemDrive = $env:SystemDrive
-$logPath = "$systemDrive\Logs\DriverMapper.log"
+
+# Install Nuget Provider, if not already installed
+Install-PackageProvider -Name "NuGet" -Force
+
+# Set PSGallery as a trusted repository
+Set-PSRepository -Name "PSGallery" -InstallationPolicy Trusted
+
+$modPoShLog = "PoShLog"
+$chkPoShLog = Get-Module -Name $modPoShLog
+
+# Check if PoShLog module is installed and if not install the current version
+if (-not $chkPoShLog) {
+    Install-Module -Name $modPoShLog -Repository "PSGallery" -Confirm:$false -Force
+}
+
+# Import PoShLog module
+Import-Module -Name $modPoShLog
+
+# Configure variables for logging
+$logFileName = "Remove-UserProfiles.log"
+$logFilePath = "$systemDrive\Logs\$logFileName"
+
+# Create logger instance
+$logger = New-Logger
+
+# Configure logger instance
+$logger |
+    Set-MinimumLevel -Value Information |
+    Add-SinkFile -Path $logFilePath |
+    Add-SinkConsole |
+    Start-Logger
+
+Write-InfoLog "PoShLog module imported and logger configured"
 
 # Splatting Variables
 $driveParams = @{
@@ -12,8 +44,10 @@ $driveParams = @{
 try {
     # Map group drive
     New-PSDrive @driveParams -ErrorAction Stop
-    Write-Output "Mapped group drive successfully." > $logPath
+    Write-InfoLog "Mapped group drive successfully."
 }
 catch {
-    Write-Error "Failed to map group drive: $_" > $logPath -ErrorAction Stop
+    Write-ErrorLog "Failed to map group drive: $_"
 }
+
+Close-Logger
