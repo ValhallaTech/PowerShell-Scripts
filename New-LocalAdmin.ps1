3a7c497f886f1cd85fed82dd18c9d64cbc8da71d
Fred <valhallatechnc@gmail.com>
2023-07-31 15:40:34 -0400

    Delete New-LocalAdmin.ps1

--- New-LocalAdmin.ps1
+++
@@ -1,7 +0,0 @@
-$user = Read-Host "Enter username"
-$password = Read-Host "Enter new password" -AsSecureString
-Write-Host "Creating $user account ..."
-New-LocalUser "$user" -Password $password -FullName "$user" -Description "$user" -Verbose
-
-# Write-Host "Adding $user to Local Administrators group ..."
-# Add-LocalGroupMember -Group "Administrators" -Member "$user" -Verbose
