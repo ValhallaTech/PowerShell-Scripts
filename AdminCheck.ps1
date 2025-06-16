766af6acd42e09f6c2a6a11a030fc777ae173108
Fred <valhallatechnc@gmail.com>
2025-06-16 15:27:49 -0400

    Create AdminCheck.ps1

---
+++ AdminCheck.ps1
@@ -0,0 +1,4 @@
+if (-not ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
+    Write-ErrorLog "Script must be run as an administrator."
+    exit 1
+}
