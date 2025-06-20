041a614dc400eaefe54a329413e8988a441be44e
Fred <valhallatechnc@gmail.com>
2025-06-20 09:09:05 -0400

    Revert "Main branch changes (#1)"
    This reverts commit 550bfe0c528a1af4a70f9ea25ece5838d014ecd2.

--- Initialize-ModuleFunction.ps1
+++ Initialize-ModuleFunction.ps1
@@ -1,9 +1,30 @@
-function Import-RequiredModule {
-    param (
-        [string]$moduleName
-    )
-    if (-not (Get-Module -ListAvailable -Name $moduleName)) {
-        Install-Module -Name $moduleName -Force -Scope CurrentUser
+function Initialize-Module ($module) {
+
+    # If module is imported say that and do nothing
+    if (Get-Module | Where-Object {$_.Name -eq $module}) {
+        write-host "Module $module is already imported."
     }
-    Import-Module -Name $moduleName -Force
+    else {
+
+        # If module is not imported, but available on disk then import
+        if (Get-Module -ListAvailable | Where-Object {$_.Name -eq $module}) {
+            Import-Module $module -Verbose
+        }
+        else {
+
+            # If module is not imported, not available on disk, but is in online gallery then install and import
+            if (Find-Module -Name $module | Where-Object {$_.Name -eq $module}) {
+                Install-Module -Name $module -Force -Verbose -Scope CurrentUser
+                Import-Module $module -Verbose
+            }
+            else {
+
+                # If the module is not imported, not available and not in the online gallery then abort
+                write-host "Module $module not imported, not available and not in an online gallery, exiting."
+                EXIT 1
+            }
+        }
+    }
 }
+
+Load-Module "ModuleName" # Use "PoshRSJob" to test it out
